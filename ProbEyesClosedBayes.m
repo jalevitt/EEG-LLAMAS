328f19bf93de16454e1dba144f304f1bcc4a2497
jalevitt <62147731+jalevitt@users.noreply.github.com>
2021-10-11 13:34:46 -0400

    Delete ProbEyesClosedBayes.m

--- ProbEyesClosedBayes.m
+++
@@ -1,9 +0,0 @@
-function [P] = ProbEyesClosedBayes(alpha, EODist, ECDist)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-pEO = normpdf(alpha, EODist.mean, EODist.stdev);
-pEC = normpdf(alpha, ECDist.mean, EODist.stdev);
-
-P = pEC/(pEO+pEC);
-end
-
