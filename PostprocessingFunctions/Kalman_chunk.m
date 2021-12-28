33239893bb7d689ed8bd8b38580e1f620f9528f9
jalevitt <62147731+jalevitt@users.noreply.github.com>
2021-12-28 13:34:54 -0500

    enable location based channel selection

--- PostprocessingFunctions/Kalman_chunk.m
+++ PostprocessingFunctions/Kalman_chunk.m
@@ -1,27 +1,36 @@
 function [vars, Graph, EEG] = Kalman_chunk(EEG,vars, Graph)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-% if vars.UseKalman
-    chunk = vars.chunk - mean(vars.chunk, 1); % re-reference to avereage
+
+    chunk = vars.chunk - mean(vars.chunk, 1); % re-reference  chunk to average
     numReg = length(vars.r);
     n = size(chunk, 2);
+    
+    % build Design Matrix
     BCG = chunk(vars.r, :);
-    %BCG = BCG_all(vars.r, :);
     DM = ones(numReg + 1, n);
-    DM(2:end, :) = BCG;
+    DM(2:end, :) = BCG - mean(BCG, 1);
     numKalman = length(vars.KalmanTargets);
     Signal = chunk(vars.KalmanTargets, :);
     h_hat = zeros(n, numKalman);
+    
+    % loop through channels and samples
     for chan = 1:numKalman
+        if  strcmp(vars.RefMode, 'Channel Locations')
+            ChannelRefs = [1, 1 + vars.RefChans(chan, :)];
+            channelDM = DM(ChannelRefs, :);
+        else
+            channelDM = DM;
+        end
         for t = 1:n
             x_hat_givenPrevious = vars.x_hat(:, chan);
             P_givenPrevious = vars.P(:, :, chan) + vars.Q;
-            KalmanGain = P_givenPrevious * DM(:, t) ...
-                * (DM(:, t)' * P_givenPrevious * DM(:, t) + vars.R)^-1;
+            KalmanGain = P_givenPrevious * channelDM(:, t) ...
+                * (channelDM(:, t)' * P_givenPrevious * channelDM(:, t) + vars.R)^-1;
             vars.x_hat(:, chan) = x_hat_givenPrevious + ...
-                KalmanGain * (Signal(chan, t) - DM(:, t)' * x_hat_givenPrevious);
-            vars.P(:, :, chan) = (eye(numReg + 1) - KalmanGain * DM(:, t)') * P_givenPrevious;
-            h_hat(t, chan) = DM(:, t)' * vars.x_hat(:, chan);
+                KalmanGain * (Signal(chan, t) - channelDM(:, t)' * x_hat_givenPrevious);
+            vars.P(:, :, chan) = (eye(numReg + 1) - KalmanGain * channelDM(:, t)') * P_givenPrevious;
+            h_hat(t, chan) = channelDM(:, t)' * vars.x_hat(:, chan);
         end
 
     end
